a0e100cef546a5afa7f90a7f299fe7ee15c57f42
thimanshu756 <himanshu1662.be22@gmail.com>
2024-08-29 19:32:49 +0530

    details card shadow bug solved

--- components/LandingPage/Common/DetailsCard.tsx
+++ components/LandingPage/Common/DetailsCard.tsx
@@ -5,7 +5,7 @@ interface DetailsCard{
 const DetailsCard = ({text}) => {
   return (
     <div className="">
-  <div className=" lg:w-[387px] rounded-2xl bg-gradient-to-r from-[#0FACFE]  to-[#A20CFF] p-[0.07rem] md:gradient-shadow ">
+  <div className=" lg:w-[387px] rounded-2xl bg-gradient-to-r from-[#0FACFE]  to-[#A20CFF] p-[0.07rem] gradient-shadow ">
     <div className="flex flex-col py-4  gap-3 h-full w-full  rounded-2xl  bg-black  ">
     <p className="px-8 text-xl text-left font-poppins  text-white">{text.title}</p>
     <p className='px-8 font-poppins'>{text.description}</p>
